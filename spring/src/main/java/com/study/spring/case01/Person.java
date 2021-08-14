be00501bcbe78c956192706c4aa57928c8429e45
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-14 14:54:26 +0800

    Spring

---
+++ spring/src/main/java/com/study/spring/case01/Person.java
@@ -0,0 +1,23 @@
+package com.study.spring.case01;
+
+public class Person {
+	private Integer ID;
+	private String name;
+	public Integer getID() {
+		return ID;
+	}
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Person [ID=" + ID + ", name=" + name + "]";
+	}
+	
+}
---
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -0,0 +1,10 @@
+package com.study.spring.case01;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
