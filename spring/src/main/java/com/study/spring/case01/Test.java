bd1b67c0433612f6282cf81f0707aff6237cee94
林丁丁 <@LAPTOP-D29KTL0G>
2021-08-14 15:11:17 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/Test.java
+++ spring/src/main/java/com/study/spring/case01/Test.java
@@ -1,9 +1,16 @@
 package com.study.spring.case01;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person person=(Person)ctx.getBean("person");
+		person.setID(1515);
+		person.setName("USER");
+		System.out.println(person);
 
 	}
 
