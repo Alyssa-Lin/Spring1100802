3fbb35262f00f2a81210a0a1419686726ae54adb
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-15 17:25:41 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest.java
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -8,6 +8,9 @@ public class MainTest {
 	public static void main(String[] args) {
 		ApplicationContext ctxApplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
+		
+		
+		
 	}
 
 }
