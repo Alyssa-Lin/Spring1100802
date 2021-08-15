9709742bf46ac9f36deb8a89456456ecf7c35620
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-15 17:34:32 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest.java
+++ spring/src/main/java/com/study/spring/case01/MainTest.java
@@ -7,7 +7,10 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+		Author author1 = ctx.getBean(Author.class);
+		Author author2 = (Author)ctx.getBean("author");
+		System.out.println(author1);
+		System.out.println(author2);
 		
 		
 		
