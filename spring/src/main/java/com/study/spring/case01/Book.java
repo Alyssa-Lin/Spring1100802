ecf0da97f0b57f97351ce84bc7f320ecc4e451f6
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-16 00:15:02 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case01/Book.java
@@ -0,0 +1,16 @@
+package com.study.spring.case01;
+
+public class Book {
+	private String name;
+	private Integer price;
+	private Author author;
+	public Book(String name, Integer price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}		
+	
+
+
+}
+
