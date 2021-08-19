8d7284e34ec45956f9be91ad8f52e7113810911e
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-19 17:31:01 +0800

    spring

--- spring/src/main/java/com/study/spring/case01/MainTest2.java
+++ spring/src/main/java/com/study/spring/case01/MainTest2.java
@@ -7,8 +7,8 @@ public class MainTest2 {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-		Husband husband=ctx.getBean(Husband.class);
-		Wife wife = ctx.getBean(Wife.class);
+		Husband husband=(Husband)ctx.getBean(Husband.class);
+		Wife wife = (Wife)ctx.getBean(Wife.class);
 	
 		System.out.printf("%s是%s的老公\n",husband.getName(),husband.getWife().getName());
 		System.out.printf("%s是%s的老婆",wife.getName(),wife.getHusband().getName());
