e0a5f27f089b7b99981472a240c1d146e1ad0b15
Alyssa-Lin <ifitrain1027@gmail.com>
2021-08-20 19:07:10 +0800

    spring

---
+++ spring/src/main/java/com/study/spring/case02/Clazz.java
@@ -0,0 +1,68 @@
+package com.study.spring.case02;
+
+public class Clazz {
+	private Integer id;
+	private String name;
+	private Integer credit;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getCredit() {
+		return credit;
+	}
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((credit == null) ? 0 : credit.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Clazz other = (Clazz) obj;
+		if (credit == null) {
+			if (other.credit != null)
+				return false;
+		} else if (!credit.equals(other.credit))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Clazz [id=" + id + ", name=" + name + ", credit=" + credit + "]";
+	}
+	
+	
+	
+	
+}
